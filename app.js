6ada46c397519fb4d1b2e7b70d57f1f4bc84cd61
root <root@dev3.azizpunjani.com>
2013-10-19 20:44:54 +0400

    Added routes to app.js. TODO: handle errors gracefully

--- app.js
+++ app.js
@@ -25,14 +25,73 @@ app.use(app.router);
 app.use(require('less-middleware')({ src: __dirname + '/public' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+var db = require('monk')('localhost/contacts'),
+    users = db.get('users'),
+    contacts = require('./contacts'); 
+
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+//Get users, with optional search query term
+app.get('/users', function(req, res) {
+    var name = req.query.name && req.query.name.split(' ') || [], 
+        query = {};  
+    
+    if (name.length === 2) {
+        query['name.first'] = new RegExp('^' + name[0], 'i'); 
+        query['name.last'] = new RegExp('^' + name[1], 'i');
+    } else if(name.length === 1) {
+        query.$or = []; 
+        query.$or.push({ 'name.first': new RegExp('^' + name[0], 'i') });
+        query.$or.push({ 'name.last': new RegExp('^' + name[0], 'i') });
+    }
+
+    users.find(query, function(err, docs){
+        res.send(docs);       
+    });
+});
+
+//Get a user by id
+app.get('/users/:id', function(req, res) {
+    var id = req.params.id, 
+    promise = users.findById(id);
+    function(err, doc) {
+        if (!doc) {
+            res.send({ success: false, error: 'Unable to find user'}); 
+        } else {
+            res.send(doc);  
+        }
+    }); 
+});
+
+//Update a user
+app.put('/users/:id', function(req, res) {
+    users.update(req.params.id, { $set: req.body }, function(){
+        res.send({ success: true });   
+    });
+});
+
+//Create a new user
+app.post('/users', function(req, res) {
+    users.insert(req.body, function(){
+        res.send({ success: true });            
+    });
+});
+
+//Delete a user
+app.del('/users/:id', function(req, res) {
+    users.remove({ _id: req.params.id }, function(){
+        res.send({ success: true });
+    });
+});
+
+//Index page
+app.get('/', function(req, res){
+    res.render('index', { title: 'Contacts Manager' });
+});
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
 });
