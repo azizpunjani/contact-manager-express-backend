3941c9e4f2c68ed63e82fc98784240c64904e18b
root <root@dev3.azizpunjani.com>
2013-10-27 04:28:12 +0400

    Changed user(s) to contact(s), moved route logic to routes file,

--- app.js
+++ app.js
@@ -4,8 +4,7 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
+  , contacts = require('./routes/contacts')
   , http = require('http')
   , path = require('path');
 
@@ -17,85 +16,29 @@ app.set('views', __dirname + '/views');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
 app.use(app.router);
 app.use(require('less-middleware')({ src: __dirname + '/public' }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-var db = require('monk')('localhost/contacts'),
-    users = db.get('users'),
-    contacts = require('./contacts'); 
 
 // development only
 if ('development' == app.get('env')) {
     app.use(express.errorHandler());
 }
 
-//Get users, with optional search query term
-app.get('/users', function(req, res) {
-    var name = req.query.name && req.query.name.split(' ') || [], 
-        query = {};  
-    
-    if (name.length === 2) {
-        query['name.first'] = new RegExp('^' + name[0], 'i'); 
-        query['name.last'] = new RegExp('^' + name[1], 'i');
-    } else if(name.length === 1) {
-        query.$or = []; 
-        query.$or.push({ 'name.first': new RegExp('^' + name[0], 'i') });
-        query.$or.push({ 'name.last': new RegExp('^' + name[0], 'i') });
-    }
-
-    users.find(query, function(err, docs){
-        res.send(docs);       
-    });
-});
+//Get contacts, with optional search query term name=first last
+app.get('/contacts', contacts.getAll);
 
 //Get a user by id
-app.get('/users/:id', function(req, res) {
-    var id = req.params.id; 
-    users.findById(id, function(err, doc) {
-        if (!doc) {
-            res.send({ success: false, error: 'Unable to find user'}); 
-        } else {
-            res.send(doc);  
-        }
-    }); 
-});
+app.get('/contacts/:id', contacts.get);
 
 //Update a user
-app.put('/users/:id', function(req, res) {
-    users.update(req.params.id, { $set: req.body }, function(err, success){
-        if (success) {
-            res.send({ success: true });
-        } else {
-            res.send({ success: false, error: 'Unable to update record'});
-        }
-    });
-});
+app.put('/contacts/:id', contacts.put);
 
 //Create a new user
-app.post('/users', function(req, res) {
-    users.insert(req.body, function(err, record){
-        if (record) {
-           res.send({ success: true, record: record });
-        } else {
-           res.send({ success: false, error: 'Unable to insert record'});
-        }
-    });
-});
+app.post('/contacts', contacts.post);
 
 //Delete a user
-app.del('/users/:id', function(req, res) {
-    users.remove({ _id: req.params.id }, function(err, success){
-        if (success) {
-           res.send({ success: true });
-        } else {
-           res.send({ success: false });
-        }
-    });
-});
+app.del('/contacts/:id', contacts.delete);
 
 //Index page
 app.get('/', function(req, res){
---
+++ routes/contacts.js
@@ -0,0 +1,66 @@
+
+/*
+ * GET contacts listing.
+ */
+var db = require('monk')('localhost/contactManager'),
+    contacts = db.get('contacts');
+
+//Get contacts, with optional search query term name=first last
+exports.getAll = function(req, res){
+    contacts.find({}, function(err, docs){
+        if (docs) {
+            res.send(docs);       
+        } else {
+            res.send({ success: false, error: 'Unable to find contacts' });
+        }
+    });
+};
+
+//Get specific contact by id
+exports.get = function(req, res){
+    var id = req.params.id; 
+    contacts.findById(id, function(err, doc) {
+        if (!doc) {
+            res.send({ success: false, error: 'Unable to find contact'}); 
+        } else {
+            res.send(doc);  
+        }
+    }); 
+};
+
+//Create a contact
+exports.post = function(req, res){
+    contacts.insert(req.body, function(err, record){
+        if (record) {
+            res.send({ success: true, record: record });
+        } else {
+            res.send({ success: false, error: 'Unable to insert record'});
+        }
+    });
+
+};
+
+//Update a contact
+exports.put = function(req, res){
+    if ( '_id' in req.body) delete req.body._id;
+    console.log(req.body);
+    contacts.update(req.params.id, req.body, function(err, success){
+        if (success) {
+            res.send({ success: true });
+        } else {
+            res.send({ success: false, error: 'Unable to update record'});
+        }
+    });
+
+};
+
+//Delete a contact
+exports.delete = function(req, res){
+    contacts.remove({ _id: req.params.id }, function(err, success){
+        if (success) {
+           res.send({ success: true });
+        } else {
+           res.send({ success: false });
+        }
+    });
+};
